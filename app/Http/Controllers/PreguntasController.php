535de866dfa3c7ad4718962c5b9b443e79203f64
ramivazquez <r.ami.vazquez@hotmail.com>
2017-11-21 14:46:05 -0300

    preguntas

---
+++ app/Http/Controllers/PreguntasController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PreguntasController extends Controller
+{
+  public function mandarAVista()
+  {
+    return view('preguntas');
+  }
+}
