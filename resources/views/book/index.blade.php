e8bc5f9e83b29f8b05c6109fec60e0c3ec60a537
alegroni <ajmarzano@gmail.com>
2017-12-15 14:23:19 -0300

    Arreglos

--- resources/views/book/index.blade.php
+++ resources/views/book/index.blade.php
@@ -11,7 +11,7 @@
   <div class="w3-teal">
     <button class="w3-button w3-teal w3-xlarge w3-hide-large" onclick="w3_open()">&#9776;</button>
     <div class="w3-container">
-      <h2>Catalogo de LongStory</h2>
+      <h2 style="text-align:center">Catalogo de LongStory</h2>
     </div>
   </div>
 
--- resources/views/book/show.blade.php
+++ resources/views/book/show.blade.php
@@ -11,23 +11,24 @@
     <div class="w3-teal">
       <button class="w3-button w3-teal w3-xlarge w3-hide-large" onclick="w3_open()">&#9776;</button>
       <div class="w3-container">
-        <h3>Detalle de Producto</h3>
+        <h3 style="text-align:center">Detalles de Producto</h3>
       </div>
     </div>
 
-    <div class="w3-container">
+    <div class="w3-container w3-center">
       <h2>Libro: {{ $libro->name }}</h2>
-      <p>descripcion corta: {{ $libro->description }}</p>
       <img src="/images/{{ $libro->image->src }}" alt="producto">
-      <ul>
+      <br><br>
+      <ul style="list-style:none">
+        <p> Descripción: {{ $libro->description }}</p>
     		<li>Rating: {{ $libro->name }}</li>
     		<li>Premios: {{ $libro->description }}</li>
     		<li>Precio $: {{ $libro->price }}</li>
-    		<li>Categoria: {{ $libro->category->name }}</li>
+    		<li>Categoría: {{ $libro->category->name }}</li>
     		<li>
-    			Actores:
+    			Autores:
     			@if (count($libro->autors) > 0)
-    				<ul>
+    				<ul style="list-style:none">
     					@foreach ($libro->autors as $autor)
     						<li>{{ $autor->name }}</li>
     					@endforeach
